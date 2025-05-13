11daa7a970e21a5b5c367e965e0fccaa6f27de60
Tausif <tausif00100@gmail.com>
2025-05-14 02:17:36 +0530

    push app folder to remote

---
+++ src/app/(protected)/home.tsx
@@ -0,0 +1,288 @@
+import {
+  Text,
+  View,
+  Image,
+  FlatList,
+  ImageBackground,
+  TouchableOpacity,
+  TextInput,
+  Alert,
+  ActivityIndicator,
+  Button,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Entypo } from '@expo/vector-icons/';
+import { categoryData, exploreData } from '~/data/data';
+import image from '~/constants/images';
+import { useState, useRef , useEffect} from 'react';
+import { prompts } from '~/constants/prompt';
+import ActionSheet, { ActionSheetRef } from 'react-native-actions-sheet';
+import images from '~/constants/images';
+import { generateAiImage, Model } from '~/ai/ai';
+import Loading from '~/components/loading-dialogue';
+// you can even create whole screen using flatlist cause it provides props like:
+// Header, footer component , empty componnet
+import { addRecipe, getLatestRecipes } from '~/appwrite/appwrite';
+import { jsonrepair } from 'jsonrepair';
+import { Redirect, router } from 'expo-router';
+import { InputItems } from 'openai/resources/responses/input-items';
+import { useAuthContext } from '~/contexts/auth-provider';
+
+const Home = () => {
+  const actionSheetRef = useRef<ActionSheetRef>(null);
+  const [inputHeight, setInputHeight] = useState(0);
+  // for storing inputs;
+  // intially value will be empty string then will
+  // change acc to function
+  const [userInput, setuserInput] = useState('');
+  // for storing generated recipe
+  const [recipeOptions, setRecipeOptions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [openLoading, setOpenLoading] = useState(false);
+  const [compeleteRecipe, setCompeleteRecipe] = useState([]);
+  const [latestRecipes, setLatestRecipes] = useState([])
+  const {user} = useAuthContext()
+
+useEffect(() => {
+ const allLatestRecipes = async() => {
+  const result = await getLatestRecipes()
+   setLatestRecipes(result)
+ }
+ allLatestRecipes()
+}, [])
+console.log("LatestRecipesfrom home.tsx :",latestRecipes);
+
+
+// console.log("USerEmail :", );
+
+
+
+
+  const press = async () => {
+    if (userInput.length < 2) {
+      Alert.alert('Error', 'Please write your food name');
+      // why return ? Return ends the program
+      // if you don't use then it will execute below of it's program
+      return;
+    }
+    try {
+      setLoading(true);
+      console.log('UserInput from Press fun :', userInput);
+      const AiResult = await Model(userInput + ' ' + prompts.GENERATE_THREE_RECIPE_PROMPT);
+      console.log('AI result from Index.tsx :', AiResult);
+      // Clean it cause it's needed to parse in JS object
+      // AI gives unwanted things with JSON like this  ```JSON``
+      //  that's why need to reapir it
+      const cleaned = jsonrepair(AiResult);
+      console.log('CleanedResult: ', cleaned);
+
+      // then parse it
+      const parsedResult = await JSON.parse(cleaned);
+      console.log('Parsed Result from Index.tsx :', parsedResult);
+      // and stor it
+      setRecipeOptions(parsedResult);
+      actionSheetRef.current?.show();
+      setLoading(false);
+    } catch (error) {
+      console.error('Error while parsing:', error);
+      Alert.alert('Error', 'AI se valid recipe data nahi mila. Try again later.');
+      setLoading(false);
+    }
+  };
+
+  console.log('recipeOptions from home.tsx :', recipeOptions);
+  console.log('userInput from home.tsx :', userInput);
+
+  const generateCompleteRecipe = async (option: any) => {
+    try {
+      actionSheetRef.current?.hide();
+      setOpenLoading(true);
+      const prompt = `recipeName: ${option.recipeName} Description: ${option.desc} ${prompts.GENERATE_COMPLETE_RECIPE}`;
+      const AiResult = await Model(prompt);
+      console.log('AI result for compelete recipe  :', AiResult);
+      // const cleaned =  AiResult.replace(/```json|```/g, '').trim();
+      const cleaned = jsonrepair(AiResult);
+      console.log('CleanedResult for compelete recipe: ', cleaned);
+      // then parse it
+      const parsedResult = await JSON.parse(cleaned);
+      console.log('Parsed Result for compelete recipe:', parsedResult);
+      // add it in state:
+      setCompeleteRecipe(parsedResult);
+      //1. Get the image prompt from complete generated Recipe
+      // await  generateRecipeImage(parsedResult[0]?.imagePrompt)
+      const aiImage = await generateAiImage(parsedResult[0]?.imagePrompt);
+      // we send it to parsedResult
+      addRecipe(parsedResult, aiImage, user?.email);
+      // addRecipe(parsedResult, aiImages)
+      setOpenLoading(false);
+    } catch (error) {
+      console.log('Error from generateCompleteRecipe :', error);
+      setOpenLoading(false);
+    }
+  };
+
+ 
+  return (
+    <SafeAreaView className="flex-1 bg-blue-300 px-4 ">
+      {/* So , in this we have three sections:
+1. Name and pfp(don't care about veg option)
+2. userInput fiedl 
+3. Category 
+4. Latest Recipies
+*/}
+
+
+      {/* For creating category*/}
+      <FlatList
+        data={categoryData}
+        showsVerticalScrollIndicator={false}
+        numColumns={4}
+        ListHeaderComponent={
+          // Firstly need a container
+          <View className="mb-6">
+            <View className=" mb-3 bg-orange-500">
+              {/* name-pfp: profile image and name */}
+              <View className=" mb-6 mt-3 flex-row items-center gap-2 ">
+                {/* If you export default images from files then for some reason you don't need require why know the reason  */}
+                <Image source={image.star} className="mb-3 h-14 w-14 rounded-full" />
+                {/* <Image source={icons.logout} resizeMode="contain" className="  h-6 w-6" /> */}
+
+                <Text className="font-poppinsRegular text-3xl font-bold">Tausif</Text>
+              </View>
+            </View>
+            <View className="  w-full rounded-2xl  bg-green-300 p-6">
+              <Text className=" mb-3 text-center font-poppinsBold   text-xl">
+                {' '}
+                Let's start cooking
+              </Text>
+              <TextInput
+                className=" mb-6    rounded-2xl bg-white  text-black"
+                style={{ height: Math.max(100, inputHeight) }}
+                multiline={true}
+                numberOfLines={4}
+                placeholder="What do you want to create? Add ingredients etc."
+                textAlignVertical="top"
+                // this for increasing size of input
+                onContentSizeChange={(e) => setInputHeight(e.nativeEvent.contentSize.height)}
+                // when user change the userInput in TextInput it will
+                // store in setInput function then useState
+                onChangeText={(text) => setuserInput(text)}
+                // why do we use value ? so that can store value of userInput in InputText
+                value={userInput}
+              />
+
+              <TouchableOpacity
+                onPress={() => press()}
+                activeOpacity={0.7}
+                className=" flex-row items-center justify-center gap-1  rounded-xl bg-green-500 p-3">
+                {!loading ? (
+                  <View className="  flex-row">
+                    <Entypo name="star" size={28} color="yellow" />
+                    <Text className="   font-poppinsSemiBold  text-lg text-white ">
+                      Generate Recipe
+                    </Text>
+                  </View>
+                ) : (
+                  <ActivityIndicator size={'large'} />
+                )}
+              </TouchableOpacity>
+            </View>
+          </View>
+        }
+        renderItem={({ item }) => (
+          // this is container
+          <TouchableOpacity 
+          onPress={() => router.push(
+            {
+              pathname:"/category", 
+              params: item
+            })}
+          activeOpacity={0.7} className="  flex-row bg-orange-500 ">
+            {/* For item */}
+            <View className="mr-20 justify-center text-center">
+              <Image
+                source={item.img}
+                resizeMode="cover"
+                className="  h-14 w-14 overflow-hidden rounded-full    "
+              />
+              <Text className=" text-center   font-poppinsSemiBold ">{item.name}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+        // So we have to add something in footer of section
+        ListFooterComponent={
+          <View className=" mt-6">
+            <Text className=" font-poppinsBold text-xl">Latest Recipes</Text>
+            {/* For creating category*/}
+            <FlatList
+              data={latestRecipes}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              // keyExtractor={(index) => index.id  }
+              renderItem={({ item }) => (
+                // this is container
+                <TouchableOpacity activeOpacity={0.7} 
+                className="  mx-2  my-2 bg-green-500"
+                onPress={() => router.push({
+                  pathname: "/details/[id]",
+                  params: item
+                })}
+               
+                >
+                  <ImageBackground
+                  source={{
+                    uri: item?.aiImage }}
+                    resizeMode="cover"
+                    resizeMethod="auto"
+                    className="h-48 w-64 overflow-hidden rounded-2xl ">
+                    <Image
+                      source={images.gradient}
+                      style={{
+                        height: '20%',
+                        width: '100%',
+                        position: 'absolute',
+                        bottom: 0,
+                        backgroundColor: '#111827',
+                      }}
+                    />
+                    <Text
+                      style={{ position: 'absolute', bottom: 4 }}
+                      numberOfLines={2}
+                      className=" w-full text-center font-poppinsBold  text-white ">
+                {item?.recipeName.length < 10 ? item?.recipeName : item?.recipeName.slice(0, 15)}...
+                </Text>
+                  </ImageBackground>
+                </TouchableOpacity>
+              )}
+            />
+          </View>
+        }
+      />
+
+      <Loading visible={openLoading} text={'Loading...'} />
+      <ActionSheet ref={actionSheetRef}>
+        <View className=" rounded-lg  px-4 py-2  ">
+          <Text className=" text-center font-poppinsBold text-lg">Select your Recipe.</Text>
+          {/* <View> */}
+          {recipeOptions?.map((item, index) => {
+            // console.log("Item from recipeOption :",item.desc);
+            return (
+              <TouchableOpacity
+                key={index}
+                onPress={() => generateCompleteRecipe(item)}
+                activeOpacity={0.8}
+                className=" mt-2  rounded-xl border   bg-green-500 p-2 ">
+                <Text className="  font-poppinsBold">{item?.recipeName}</Text>
+                <Text className=" font-poppinsRegular">{item?.desc}</Text>
+              </TouchableOpacity>
+            );
+          })}
+          {/* </View> */}
+        </View>
+      </ActionSheet>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
+
---
+++ src/app/index.tsx
@@ -0,0 +1,109 @@
+import { Alert, Button, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+
+import { Redirect, router } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { Marquee } from '@animatereactnative/marquee';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { AntDesign } from '@expo/vector-icons';
+import { createUser } from '../appwrite/appwrite';
+import { allImages, exploreData } from '~/data/data';
+import { useAuthContext } from '~/contexts/auth-provider';
+
+const Index = () => {
+  const { loggedIn, user } = useAuthContext();
+  // if (!loggedIn && user) {
+  // It's very bad code cause when loggedIn true then it doesn't let naviage to home screen
+  // instead if loggedIn is false then don't navigate 
+  if (loggedIn && user) {
+    return <Redirect href="/home" />;
+  }
+
+  // This fun will login the user 
+  const loginUser = async () => {
+    try {
+      // will call the createUser function 
+      // and store the return value in user variable 
+      const user = await createUser();
+      // Then push the user to home screeen 
+      router.push('/home');
+    } catch (error) {
+ 
+      console.error("Error in loginUser fun from index.tsx :",error);
+
+      Alert.alert('Auth Error', 'Failed to Authorize');
+
+      throw new Error('Failed to Authorize')
+
+    }
+  };
+
+  return (
+    <SafeAreaView className="flex-1   bg-blue-300 px-4 ">
+      <StatusBar backgroundColor="white" />
+
+      {/* Container for animations */}
+      <View className=" mb-6 mt-3">
+
+{/* By marquee componet let the items animate from right to left 
+for 3 horizontal items  */}
+        <Marquee
+          spacing={10}
+          speed={0.9}
+          style={{
+            transform: [{ rotate: '-4deg' }],
+          }}>
+          <View className="flex flex-row gap-6">
+            {allImages.map((item, index) => (
+              <Image key={index} source={item} className=" h-40 w-40 rounded-xl" />
+            ))}
+          </View>
+        </Marquee>
+
+        <Marquee
+          spacing={10}
+          speed={0.7}
+          style={{
+            transform: [{ rotate: '-4deg' }],
+            marginTop: 10,
+          }}>
+          <View className="flex flex-row gap-6">
+            {allImages.map((item, index) => (
+              <Image key={index} source={item} className=" h-40 w-40 rounded-xl" />
+            ))}
+          </View>
+        </Marquee>
+        <Marquee
+          spacing={10}
+          speed={0.5}
+          style={{
+            transform: [{ rotate: '-4deg' }],
+            marginTop: 10,
+          }}>
+          <View className="flex flex-row gap-6">
+            {allImages.map((item, index) => (
+              <Image key={index} source={item} className=" h-40 w-40 rounded-xl" />
+            ))}
+          </View>
+        </Marquee>
+      </View>
+
+      {/* App Info and Login container*/}
+      <View className="  h-56 items-center  justify-center rounded-xl bg-green-500   p-5 ">
+        <Text className="  font-poppinsBold  text-custom-green  text-2xl">Cookmate AI</Text>
+        <Text className="   font-semibold ">Generate delicious recipes in seconds</Text>
+        <Text className="   font-semibold text-gray-600">with the power of AI</Text>
+
+        <TouchableOpacity
+      
+          onPress={loginUser}
+          activeOpacity={0.7}
+          className="   bg-custom-green w-full flex-row items-center justify-center gap-2 rounded-xl p-2  p-3 ">
+          <AntDesign name="google" size={24} color="black" />
+          <Text className="  text-base   font-semibold">Continue with Google</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Index;
